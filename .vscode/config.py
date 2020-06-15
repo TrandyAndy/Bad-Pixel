e8950dfb30b8705a06f74f932dfe3e9ff05bdf78
JLS666 <43670180+JLS666@users.noreply.github.com>
2020-06-15 16:14:26 +0200

    Anleitung für Andy

--- .vscode/config.py
+++ .vscode/config.py
@@ -11,3 +11,10 @@ Farbtiefe=16 #in Bit
 #BPM= array([Bildbreite][Bildhoehe]) #Bad Pixel Map
 #ImageArr= array([Bildbreite][Bildhoehe][Bilderzahl])
 BildCounter=0
+
+"""
+
+    B = np.array([ [[111, 112], [121, 122]],
+               [[211, 212], [221, 222]],
+               [[311, 312], [321, 322]] ])  # 3D Array
+"""
