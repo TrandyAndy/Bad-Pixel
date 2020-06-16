dfabddab0c48c4c3a0e1a32fc93433c8f20a6399
JLS666 <43670180+JLS666@users.noreply.github.com>
2020-06-17 01:57:48 +0200

    Andy soll das plus suchen

--- .vscode/markPixels.py
+++ .vscode/markPixels.py
@@ -3,11 +3,15 @@ import numpy as np
 
 def markPixels(bpm, bild):
     backtorgb = cv2.cvtColor(bild,cv2.COLOR_GRAY2RGB)
-    backtorgb[0,0,1] = 65535
-    cv2.imshow('image', backtorgb)
+    # backtorgb[Zeile: 0, Spalte: 0, rgb: 1] = maximaler Wert: 65535
+    backtorgb[100,201,1] = 65535
+    backtorgb[101,200,1] = 65535
+    backtorgb[101,202,1] = 65535
+    backtorgb[102,201,1] = 65535
+    #cv2.imshow('image', backtorgb)
     print(backtorgb)
     cv2.imwrite('redPixels.png', backtorgb, [cv2.IMWRITE_PNG_COMPRESSION,0])
-    cv2.waitKey()
-    cv2.destroyAllWindows()
+    #cv2.waitKey()
+    #cv2.destroyAllWindows()
     
 
