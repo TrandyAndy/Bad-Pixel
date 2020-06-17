92eeba91ca8e39286dbc9f30e7643ccf223a9733
JLS666 <43670180+JLS666@users.noreply.github.com>
2020-06-17 03:51:15 +0200

    Letzte Änderungen für dem Tag.

--- .vscode/badpixel.py
+++ .vscode/badpixel.py
@@ -46,5 +46,5 @@ TestImage=np.array([ [[111, 65535], [121, 65535]],
 #detection.MultiPicturePixelCompare(TestImage)
 
 
-mP.markPixels(detection.advancedMovingWindow(bildDaten, 0,6)[0],bildDaten[0])
-
+#mP.markPixels(detection.advancedMovingWindow(bildDaten, 0,6)[0],bildDaten[0])
+mP.markPixels(detection.movingWindow(bildDaten[0]),bildDaten[0])
