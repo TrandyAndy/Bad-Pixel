175c1d1e2ae526c4f01b1f4f010b979f48c0a2a0
TrandyAndy <43793912+TrandyAndy@users.noreply.github.com>
2020-07-27 15:48:39 +0200

    aufräumen um Version 2.0 zu beginnen

---
+++ .vscode/Hauptfunktion.py
@@ -0,0 +1,26 @@
+ 
+"""
+/*
+ * @Author: Julian Schweizerhof Andreas Bank
+ * @Email: diegruppetg@gmail.com
+ * @Date: 2020-07_27
+ * @Last Modified by: 
+ * @Last Modified time: 2020-06-21 01:32:30
+ * @Description: main
+ */
+ """
+         
+#import matplotlib.pyplot as plt
+import numpy as np
+#how to import numpy und ov2.  Python updaten, pip install, pip3 install numpy, pip3 install opencv-python.
+
+import detection
+import correction
+import config
+import importPictures as imP
+import cv2
+import telemetry
+import cProfile 
+import verpixler
+
+
